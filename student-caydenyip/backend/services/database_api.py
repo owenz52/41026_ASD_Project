145022f06e333712964a6241be397cdea12c5164
Cayden194 <cayden.a.yip@student.uts.edu.au>
2026-09-02 16:26:08 +1000

    Working enrolment connetcion and table display

--- student-caydenyip/backend/services/database_api.py
+++ student-caydenyip/backend/services/database_api.py
@@ -7,17 +7,43 @@ DATABASE_SERVICE_URL = os.getenv(
     "http://exam-database:5010"
 )
 
+ENROLMENT_SERVICE_URL = os.getenv(
+    "ENROLMENT_SERVICE_URL",
+    "http://enrolment-database:5002"
+)
 
-def get_exams():
+
+
+def get_student_enrolments(student_id):
+
     response = requests.get(
-        f"{DATABASE_SERVICE_URL}/exams",
+        f"{ENROLMENT_SERVICE_URL}/enrolments/by-student/{student_id}",
         timeout=5
     )
+
     response.raise_for_status()
+
+    return response.json()
+
+
+
+def get_exams(student_id):
+
+    response = requests.get(
+        f"{DATABASE_SERVICE_URL}/exams",
+        params={
+            "student_id": student_id
+        },
+        timeout=5
+    )
+
+    response.raise_for_status()
+
     return response.json()
 
 
 def get_exam_by_id_response(exam_id):
+
     return requests.get(
         f"{DATABASE_SERVICE_URL}/exams/{exam_id}",
         timeout=5
@@ -25,14 +51,18 @@ def get_exam_by_id_response(exam_id):
 
 
 def get_exams_by_course_response(course_id):
+
     return requests.get(
         f"{DATABASE_SERVICE_URL}/exams/by-course",
-        params={"course_id": course_id},
-        timeout=5,
+        params={
+            "course_id": course_id
+        },
+        timeout=5
     )
 
 
 def update_exam_response(exam_id, data):
+
     return requests.put(
         f"{DATABASE_SERVICE_URL}/exams/{exam_id}",
         json=data,
@@ -41,7 +71,44 @@ def update_exam_response(exam_id, data):
 
 
 def delete_exam_response(exam_id):
+
     return requests.delete(
         f"{DATABASE_SERVICE_URL}/exams/{exam_id}",
         timeout=5
     )
+
+
+# ==================================================
+# RESET EXAMS FOR STUDENT
+# ==================================================
+
+def reset_exams_response(student_id, course_ids):
+
+    return requests.post(
+        f"{DATABASE_SERVICE_URL}/exams/reset",
+        params={
+            "student_id": student_id
+        },
+        json={
+            "student_id": student_id,
+            "course_ids": course_ids
+        },
+        timeout=5
+    )
+# ==================================================
+# SYNC STUDENT EXAMS
+# ==================================================
+
+def sync_exams_response(student_id, course_ids):
+
+    return requests.post(
+        f"{DATABASE_SERVICE_URL}/exams/sync",
+        params={
+            "student_id": student_id
+        },
+        json={
+            "course_ids": course_ids
+        },
+        timeout=5
+    )
+
